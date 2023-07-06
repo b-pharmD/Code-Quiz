520e1ab8d7ea86b99ad109660801e99a2db553e3
bburr <bradleyjamesburr@gmail.com>
2023-07-05 23:29:50 -0600

    changed quiz question HTML to radio, changed questions to single array in JS

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,49 @@
-
-var question1 = {
-    question: "Which of the following is used to select a class in CSS?",
-    choices: [".", "$", "#", "<>"],
-    answer: "."
-}
-
-var question2 = {
-    question: "Which of the folowing is used to select an id in CSS?",
-    choices: [".", "$", "#", "<>"],
-    answer: "#"
-}
-
-var question3 = {
-    question: "Primitive Data Types include:",
-    choices: ["String", "Number", "Boolean", "All of the above"],
-    answer: "All of the above"
-}
-
-var question4 = {
-    question: "Which operator returns the remainder between two numbers?",
-    choices: ["&", "%", "~", "/"],
-    answer: "%"
-}
-
-var question5 = {
-    question: "What can be stored in objects?",
-    choices: ["Primitive data", "Arrays", "Methods", "All of the above"],
-    answer: "All of the above"
-}
-
-var question6 = {
-    question: "Which is not part of the CSS box model?",
-    choices: ["Margin", "Border", "Padding", "Window"],
-    answer: "Window"
-}
-
-var question7 = {
-    question: "Which of the following is not an array method?",
-    choices: ["valueOf()", "pop()", "push()", "concat()"],
-    answer: "valueOf()"
-}
+var questions = [
+    {
+        question: "Which of the following is used to select a class in CSS?",
+        choices: [".", "$", "#", "<>"],
+        answer: "."
+    },
+    {
+        question: "Which of the folowing is used to select an id in CSS?",
+        choices: [".", "$", "#", "<>"],
+        answer: "#"
+    },
+    {
+        question: "Primitive Data Types include:",
+        choices: ["String", "Number", "Boolean", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        question: "Which operator returns the remainder between two numbers?",
+        choices: ["&", "%", "~", "/"],
+        answer: "%"
+    },
+    {
+        question: "What can be stored in objects?",
+        choices: ["Primitive data", "Arrays", "Methods", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        question: "Which is not part of the CSS box model?",
+        choices: ["Margin", "Border", "Padding", "Window"],
+        answer: "Window"
+    },
+    {
+        question: "Which of the following is not an array method?",
+        choices: ["valueOf()", "pop()", "push()", "concat()"],
+        answer: "valueOf()"
+    }];
 
 var timer = document.getElementById('time')
 var timeLeft = 60;
 
 function countdown() {
-    var timeInterval = setInterval(function() {
+    var timeInterval = setInterval(function () {
         timeLeft--;
         timer.textContent = timeLeft
 
-        if(timeLeft === 0){
+        if (timeLeft === 0) {
             clearInterval(timeInterval);
         }
 
